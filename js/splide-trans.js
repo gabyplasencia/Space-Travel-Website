a4ac6e315d603571af559c90070a6dcaa3e7b417
Gabriela <gabyplasencia1995@gmail.com>
2023-09-11 19:21:32 +0100

    the transport html struture and the js needed for the carrousel

---
+++ js/splide-trans.js
@@ -0,0 +1,19 @@
+document.addEventListener( 'DOMContentLoaded', function() {
+
+    var splide = new Splide( '.splide', {
+      arrows: false,
+      rewind: true,
+      type: 'fade',
+    } );
+    
+    //This allows to add a number to each dot
+    splide.on( 'pagination:mounted', function ( data ) {
+      data.list.classList.add( 'splide__pagination--custom' );
+      data.items.forEach( function ( item ) {
+        item.button.textContent = String( item.page + 1 );
+      } );
+    } );
+
+    
+    splide.mount();
+  } );
