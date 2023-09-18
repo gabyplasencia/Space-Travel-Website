c1bff4a22f15201a765d93b61224cc092f43aeaf
Gabriela <gabyplasencia1995@gmail.com>
2023-09-18 12:54:19 +0100

    destination tabs now are funtional

--- js/nav.js
+++ js/nav.js
@@ -1,20 +1,20 @@
-const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".main-navbar");
+const navToggle = document.querySelector('.nav-toggle');
+const navMenu = document.querySelector('.main-navbar');
 
-navToggle.addEventListener("click", () => {
+navToggle.addEventListener('click', () => {
       
      openAndClose();
 })
 
 const openAndClose = () => {
 
-    if (navMenu.classList.contains("menu-open")) {
-        navToggle.classList.add("menu-close");
-        navMenu.classList.remove("menu-open");
-        navToggle.classList.remove("menu-open");
+    if (navMenu.classList.contains('menu-open')) {
+        navToggle.classList.add('menu-close');
+        navMenu.classList.remove('menu-open');
+        navToggle.classList.remove('menu-open');
       } else {
-        navToggle.classList.remove("menu-close");
-        navMenu.classList.add("menu-open");
-        navToggle.classList.add("menu-open");
+        navToggle.classList.remove('menu-close');
+        navMenu.classList.add('menu-open');
+        navToggle.classList.add('menu-open');
       }
 }
--- js/tabs.js
+++ js/tabs.js
@@ -1,16 +1,44 @@
-const tabs = document.querySelectorAll('[data-tab-target]')
-const tabContents = document.querySelectorAll('[data-tab-content]')
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
+
 
 tabs.forEach(tab => {
   tab.addEventListener('click', () => {
-    const target = document.querySelector(tab.dataset.tabTarget)
+    const target = document.querySelector(tab.dataset.tabTarget);
     tabContents.forEach(tabContent => {
-      tabContent.classList.remove('active')
+      tabContent.classList.remove('tab-active');
     })
     tabs.forEach(tab => {
-      tab.classList.remove('active')
+      tab.classList.remove('tab-active');
     })
-    tab.classList.add('active')
-    target.classList.add('active')
+    tab.classList.add('tab-active'); //display the text content
+    target.classList.add('tab-active'); //select the correct tab
+
+    const id = target.id;
+    
+    switch (id) {
+      case 'moon':
+        const moon = document.querySelector('.moon');
+        moon.classList.add('tab-active');
+        break;
+
+      case 'mars':
+        const mars = document.querySelector('.mars');
+        mars.classList.add('tab-active');
+        break;
+
+      case 'europa':
+        const europa = document.querySelector('.europa');
+        europa.classList.add('tab-active');
+        break;
+
+      case 'titan':
+        const titan = document.querySelector('.titan');
+        titan.classList.add('tab-active');
+        break;
+
+      default:
+        moon.classList.add('tab-active');
+    }
   })
 })
